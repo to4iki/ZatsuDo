77b3bb964875340b636648c4159f951e777fc7a6
Toshiki Takezawa <tsk.take815@gmail.com>
2026-03-29 10:10:30 +0900

    test(TaskFeature): minimize TaskListViewModelTests to essential coverage

--- AppPackage/Tests/Feature/Task/TaskListViewModelTests.swift
+++ AppPackage/Tests/Feature/Task/TaskListViewModelTests.swift
@@ -13,16 +13,9 @@ struct TaskListViewModelTests {
     viewModel.addTask()
 
     let taskId = viewModel.uiState.tasks[0].id
-
-    #expect(viewModel.uiState.tasks[0].isDone == false)
-
     viewModel.toggleTask(id: taskId)
 
     #expect(viewModel.uiState.tasks[0].isDone == true)
-
-    viewModel.toggleTask(id: taskId)
-
-    #expect(viewModel.uiState.tasks[0].isDone == false)
   }
 
   @Test
@@ -32,27 +25,21 @@ struct TaskListViewModelTests {
     viewModel.updateInputText("Buy milk")
     viewModel.addTask()
 
-    let invalidId = ZatsuTask.ID(rawValue: "nonexistent")
+    viewModel.toggleTask(id: ZatsuTask.ID(rawValue: "nonexistent"))
 
-    viewModel.toggleTask(id: invalidId)
-
-    #expect(viewModel.uiState.tasks.count == 1)
     #expect(viewModel.uiState.tasks[0].isDone == false)
   }
 
   @Test
-  func addTask_appendsNewTask() {
+  func addTask_appendsTaskAndClearsInput() {
     let viewModel = TaskListViewModel()
 
     viewModel.updateInputText("Task 1")
     viewModel.addTask()
 
-    viewModel.updateInputText("Task 2")
-    viewModel.addTask()
-
-    #expect(viewModel.uiState.tasks.count == 2)
+    #expect(viewModel.uiState.tasks.count == 1)
     #expect(viewModel.uiState.tasks[0].name == "Task 1")
-    #expect(viewModel.uiState.tasks[1].name == "Task 2")
+    #expect(viewModel.uiState.inputText == "")
   }
 
   @Test
@@ -60,37 +47,10 @@ struct TaskListViewModelTests {
     let viewModel = TaskListViewModel()
 
     viewModel.addTask()
-
     #expect(viewModel.uiState.tasks.isEmpty)
-  }
 
-  @Test
-  func addTask_trimsWhitespace() {
-    let viewModel = TaskListViewModel()
-
-    viewModel.updateInputText("  Buy milk  ")
+    viewModel.updateInputText("   ")
     viewModel.addTask()
-
-    #expect(viewModel.uiState.tasks.count == 1)
-    #expect(viewModel.uiState.tasks[0].name == "Buy milk")
-  }
-
-  @Test
-  func addTask_clearsInputText() {
-    let viewModel = TaskListViewModel()
-
-    viewModel.updateInputText("Buy milk")
-    viewModel.addTask()
-
-    #expect(viewModel.uiState.inputText == "")
-  }
-
-  @Test
-  func updateInputText_updatesState() {
-    let viewModel = TaskListViewModel()
-
-    viewModel.updateInputText("Hello")
-
-    #expect(viewModel.uiState.inputText == "Hello")
+    #expect(viewModel.uiState.tasks.isEmpty)
   }
 }
